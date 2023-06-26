0d6d61cd4cb0264f9811d5614366ad981342fd56
Janosos <126541994+Janosos@users.noreply.github.com>
2023-06-26 16:21:23 -0600

    Initial commit

---
+++ ProyectoOrdenamiento/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoOrdenamiento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            Console.WriteLine($"Arreglo generado al azar de longitud {n}:");
+            int[] arreglo = new int[n];
+            Random random = new Random();
+            for ( int i = 0; i < arreglo.Length; i++ )
+                arreglo[i] = random.Next(1,1000);
+            PrintArray(arreglo);
+
+            Console.WriteLine("Arreglo ordenado:");
+            PrintArray(Sort.QuickSort(arreglo));
+
+            Console.ReadLine();
+        }
+
+        private static void PrintArray(int[] array)
+        {
+            Console.Write("[");
+            for ( int i = 0;i < array.Length;i++ )
+            {
+                Console.Write(array[i]);
+                if ( i < array.Length - 1 )
+                    Console.Write(",");
+            }
+            Console.Write("]\n");
+        }
+    }
+}
---
+++ ProyectoOrdenamiento/Sort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoOrdenamiento
+{
+    internal class Sort
+    {
+        public static int[] QuickSort(int[] arreglo)
+        {
+            // Copia el arreglo, con el fin de consevar el arreglo desordenado
+            int[] copia = new int[arreglo.Length];
+            Array.Copy(arreglo,copia,arreglo.Length);
+            QuickSort(copia, 0, arreglo.Length - 1);
+            return copia;
+        }
+
+        private static void QuickSort(int[] arreglo, int izquierda, int derecha)
+        {
+            int i = izquierda;
+            int j = derecha;
+            int pivote = arreglo[izquierda];
+
+            while ( i <= j )
+            {
+                while (arreglo[i] < pivote)
+                {
+                    i++;
+                }
+                while (arreglo[j] > pivote)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    (arreglo[j], arreglo[i]) = (arreglo[i], arreglo[j]);
+                    i++;
+                    j--;
+                }
+            }
+            if (izquierda < j)
+                QuickSort(arreglo, izquierda, j);
+            if (i < derecha)
+                QuickSort(arreglo, i, derecha);
+        }
+    }
+}
